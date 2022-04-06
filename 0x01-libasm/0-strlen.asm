59c3bf33450df64800d6e63442556d13e7b103d5
dreeseh <1859@holbertonschool.com>
2022-04-06 20:41:45 +0000

    my other version of strlen

--- 0x01-libasm/0-strlen.asm
+++ 0x01-libasm/0-strlen.asm
@@ -1,14 +1,23 @@
-global	strlen
-	; int strlen(const char *string);
-strlen:
-	push	edi
-	sub	ecx, ecx
-	mov	edi, [esp+8]
-	not	ecx
-	sub	al, al
-	cld
-repne	scasb
-	not	ecx
-	pop	edi
-	lea	eax, [ecx-1]
+BITS 64
+
+	global asm_strlen
+
+	section .text
+
+asm_strlen:
+	push rbp
+	mov rbp, rsp
+	push rcx
+
+	xor rcx, rcx
+loop_asm_strlen:
+	cmp byte [rdi + rcx], 0
+	je end
+	inc rcx
+	jmp loop_asm_strlen
+end:
+	mov rax, rcx
+	pop rcx
+	mov rsp, rbp
+	pop rbp
 	ret
