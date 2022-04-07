ba3e96e47897acc5b87d43f3aa9a6434f095daba
dreeseh <1859@holbertonschool.com>
2022-04-07 14:54:35 +0000

    removed all PTR's

--- 0x01-libasm/1-strcmp.asm
+++ 0x01-libasm/1-strcmp.asm
@@ -1,32 +1,32 @@
 strcmp:
         push    rbp
         mov     rbp, rsp
-        mov     QWORD PTR [rbp-8], rdi
-        mov     QWORD PTR [rbp-16], rsi
-        jmp     .L2
-.L5:
-        mov     rax, QWORD PTR [rbp-8]
-        movzx   edx, BYTE PTR [rax]
-        mov     rax, QWORD PTR [rbp-16]
-        movzx   eax, BYTE PTR [rax]
+        mov     QWORD [rbp-8], rdi
+        mov     QWORD [rbp-16], rsi
+        jmp     L2
+L5:
+        mov     rax, QWORD [rbp-8]
+        movzx   edx, BYTE [rax]
+        mov     rax, QWORD [rbp-16]
+        movzx   eax, BYTE [rax]
         cmp     dl, al
-        jne     .L7
-        add     QWORD PTR [rbp-8], 1
-        add     QWORD PTR [rbp-16], 1
-.L2:
-        mov     rax, QWORD PTR [rbp-8]
-        movzx   eax, BYTE PTR [rax]
+        jne     L7
+        add     QWORD [rbp-8], 1
+        add     QWORD [rbp-16], 1
+L2:
+        mov     rax, QWORD [rbp-8]
+        movzx   eax, BYTE [rax]
         test    al, al
-        jne     .L5
-        jmp     .L4
-.L7:
+        jne     L5
+        jmp     L4
+L7:
         nop
-.L4:
-        mov     rax, QWORD PTR [rbp-8]
-        movzx   eax, BYTE PTR [rax]
+L4:
+        mov     rax, QWORD [rbp-8]
+        movzx   eax, BYTE [rax]
         movzx   edx, al
-        mov     rax, QWORD PTR [rbp-16]
-        movzx   eax, BYTE PTR [rax]
+        mov     rax, QWORD [rbp-16]
+        movzx   eax, BYTE [rax]
         movzx   eax, al
         sub     edx, eax
         mov     eax, edx
