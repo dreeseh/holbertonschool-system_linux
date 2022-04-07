ae158f483a0ad9183c1f37263fce426999082e53
dreeseh <1859@holbertonschool.com>
2022-04-07 15:12:58 +0000

    rework using checks

--- 0x01-libasm/1-strcmp.asm
+++ 0x01-libasm/1-strcmp.asm
@@ -1,39 +1,39 @@
 BITS 64
 
-global asm_strcmp
+	global asm_strcmp
 
-section .text
+	section .text
+
 asm_strcmp:
-        push    rbp
-        mov     rbp, rsp
-        mov     QWORD [rbp-8], rdi
-        mov     QWORD [rbp-16], rsi
-        jmp     L2
-L5:
-        mov     rax, QWORD [rbp-8]
-        movzx   edx, BYTE [rax]
-        mov     rax, QWORD [rbp-16]
-        movzx   eax, BYTE [rax]
-        cmp     dl, al
-        jne     L7
-        add     QWORD [rbp-8], 1
-        add     QWORD [rbp-16], 1
-L2:
-        mov     rax, QWORD [rbp-8]
-        movzx   eax, BYTE [rax]
-        test    al, al
-        jne     L5
-        jmp     L4
-L7:
-        nop
-L4:
-        mov     rax, QWORD [rbp-8]
-        movzx   eax, BYTE [rax]
-        movzx   edx, al
-        mov     rax, QWORD [rbp-16]
-        movzx   eax, BYTE [rax]
-        movzx   eax, al
-        sub     edx, eax
-        mov     eax, edx
-        pop     rbp
-        ret
+	push	rbp
+	mov	rbp,	rsp
+	push	rdx
+
+loop:
+	mov	dl,	[rdi]
+	mov	dh,	[rsi]
+	cmp	dl,	dh
+	jl	lessthan
+	jg 	greaterthan
+	cmp	dl,	0h
+	je 	check
+	inc	rdi
+	inc	rsi
+	jmp	loop
+
+lessthan:
+	mov	eax,	-1h
+	jmp	out
+
+greaterthan:
+	mov	eax,	1h
+	jmp	out
+
+check:
+	mov	eax,	0h
+
+out:
+	pop	rdx
+	mov	rsp,	rbp
+	pop	rbp
+	ret
