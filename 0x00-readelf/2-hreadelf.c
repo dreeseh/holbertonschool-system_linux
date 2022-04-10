54f2c37e301cd25c82a454da533bfe322faa172e
dreeseh <1859@holbertonschool.com>
2022-04-10 23:45:11 +0000

    new Makefile, new .c file & prev makefiles

---
+++ 0x00-readelf/2-hreadelf.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point for program
+ * @argc: the number of args
+ * @argv: the arg vector
+ * @env:  the environment variable
+ * Return: -1 on fail, 0 upon success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	(void)argc;
+	args[3] = argv[1];
+
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	
+	return (EXIT_SUCCESS);
+}
