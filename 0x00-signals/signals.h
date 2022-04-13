543e136ed7f29c9053f8b305c88bb8762b65ceea
dreeseh <1859@holbertonschool.com>
2022-04-13 15:33:43 +0000

    linter fixes on header file

--- 0x00-signals/signals.h
+++ 0x00-signals/signals.h
@@ -7,18 +7,22 @@
 #include <string.h>
 #include <unistd.h>
 
-/** prototypes**/
+/**	prototypes **/
+/*-------------------------------*/
+/*	task 0 */
 int handle_signal(void);
 void signal_handler(int sig_num);
 
+/*	task 1 */
 void (*current_handler_signal(void))(int);
 
+/*	task 2 */
 int handle_sigaction(void);
 
+/*	task 3 */
 void (*current_handler_sigaction(void))(int);
 
+/*	task 4 */
 int trace_signal_sender(void);
-
-
 
 #endif /** _SIGNALS_H_ **/
