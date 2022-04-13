7e605867bb6fb21b3d8c1b4e85e53c223950573c
dreeseh <1859@holbertonschool.com>
2022-04-13 20:35:34 +0000

    files for task 10

---
+++ 0x00-signals/10-pid_exist.c
@@ -0,0 +1,11 @@
+#include "signals.h"
+
+/**
+ * pid_exist - tests if a process exists, given its PID
+ * @pid: the PID we care checking
+ * Return: the PID or 0 on fail
+ */
+int pid_exist(pid_t pid)
+{
+	return (!kill(pid, 0));
+}
--- 0x00-signals/signals.h
+++ 0x00-signals/signals.h
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 /**---------------------**/
 /**	prototypes	**/
