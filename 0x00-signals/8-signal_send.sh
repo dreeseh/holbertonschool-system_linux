9323ff5571edbd1c9224e80722cb9a7a727d63f3
dreeseh <1859@holbertonschool.com>
2022-04-13 20:12:30 +0000

    moved then up a line

--- 0x00-signals/8-signal_send.sh
+++ 0x00-signals/8-signal_send.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #sends the signal SIGQUIT to a process, given its PID
 
-if [ "$#" -ne 1 ];
-then
-    echo "Usage: $0 <pid>"
-    exit 1
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <pid>"
+	exit 1
 else
-    kill -QUIT $1
+	kill -QUIT $1
 fi
