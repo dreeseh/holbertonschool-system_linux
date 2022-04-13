74600e3617a28babf71210de08676073d8f02da9
dreeseh <1859@holbertonschool.com>
2022-04-13 14:12:38 +0000

    task 0

---
+++ 0x00-signals/0-handle_signal.c
@@ -0,0 +1,23 @@
+#include "signals.h"
+
+/**
+ * signal_handler - handles SIGINT signal
+ * Return: is void
+ */
+void signal_handler(int sig_num)
+{
+	printf("Gotcha! [%d]\n", sig_num);
+	fflush(stdout);
+}
+
+/**
+ * handle_signal - 
+ * Return: 0 on success, or -1 on error
+ */
+int handle_signal(void)
+{
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+		return (-1);
+	
+	return (0);
+}
