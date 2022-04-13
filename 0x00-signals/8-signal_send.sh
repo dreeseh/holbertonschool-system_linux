3106bb7e61f6da6cfe1b72be300e510535d77d30
dreeseh <1859@holbertonschool.com>
2022-04-13 19:53:41 +0000

    added fi to end of function

--- 0x00-signals/8-signal_send.sh
+++ 0x00-signals/8-signal_send.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #sends the signal SIGQUIT to a process, given its PID
 
-if [ "$#" -ne 1 ];
+if [ "$#" -ne 1 ]
 then
-	echo "Usage: $0 <pid>"
-	exit 1
+    echo "Usage: $0 <pid>"
+    exit 1
+else
+    kill -QUIT $1
 fi
-kill -QUIT $1
