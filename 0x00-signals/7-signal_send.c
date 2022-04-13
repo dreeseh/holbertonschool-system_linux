43ee9bfb4cd14bb899c32363d55caedb1de7a5aa
dreeseh <1859@holbertonschool.com>
2022-04-13 19:33:03 +0000

    task 7

---
+++ 0x00-signals/7-signal_send.c
@@ -0,0 +1,23 @@
+#include "signals.h"
+
+/**
+ * @brief 
+ * 
+ */
+int main(int argc, char **argv)
+{
+	int pid_number = 0;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	pid_number = atoi(argv[1]);
+	if (kill(pid_number, SIGINT) == 0)
+		return (EXIT_SUCCESS);
+
+	return (EXIT_FAILURE);
+
+}
