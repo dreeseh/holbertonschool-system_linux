a8739533d2d0308e92bb1f167cf0bfce3af29a23
dreeseh <1859@holbertonschool.com>
2022-04-14 14:37:59 +0000

    files for adv task 12

---
+++ 0x00-signals/100-all_in_one.c
@@ -0,0 +1,34 @@
+#include "signals.h"
+
+/**
+ * all_in_one_sigHandler - signal handler for the below function
+ * @sig: number of the signal that caused invocation
+ * @info: pointer to a siginfo_t
+ * @ucontext: a pointer to a ucontext_t structure, cast to void
+ * Return: is void
+ */
+void all_in_one_sigHandler(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) sig;
+	(void) ucontext;
+
+	psiginfo(info, "Caught");
+}
+
+/**
+ * all_in_one - sets up a single handler for all the signals
+ * Return: is void
+ */
+void all_in_one(void)
+{
+	int sig = 0;
+	struct sigaction sig_action;
+
+	sig_action.sa_flags = SA_SIGINFO;
+	sig_action.sa_sigaction = all_in_one_sigHandler;
+	while (sig <= SIGRTMAX)
+	{
+		sigaction(sig, &sig_action, NULL);
+		sig++;
+	}
+}
