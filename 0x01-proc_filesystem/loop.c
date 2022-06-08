fe8080100163143976fa971eb8b87e7d46592a1a
dreeseh <1859@holbertonschool.com>
2022-06-08 16:10:02 +0000

    betty fixes

--- 0x01-proc_filesystem/loop.c
+++ 0x01-proc_filesystem/loop.c
@@ -3,28 +3,28 @@
 #include <string.h>
 #include <unistd.h>
 
-/**              
+/**
  * main - uses strdup to create a new string, loops forever-ever
- *                
+ *
  * Return: EXIT_FAILURE if malloc failed. Other never returns
  */
 int main(void)
 {
-     char *s;
-     unsigned long int i;
+	char *s;
+	unsigned long int i;
 
-     s = strdup("Holberton");
-     if (s == NULL)
-     {
-          fprintf(stderr, "Can't allocate mem with malloc\n");
-          return (EXIT_FAILURE);
-     }
-     i = 0;
-     while (s)
-     {
-          printf("[%lu] %s (%p)\n", i, s, (void *)s);
-          sleep(1);
-          i++;
-     }
-     return (EXIT_SUCCESS);
+	s = strdup("Holberton");
+	if (s == NULL)
+	{
+		fprintf(stderr, "Can't allocate mem with malloc\n");
+		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while (s)
+	{
+		printf("[%lu] %s (%p)\n", i, s, (void *)s);
+		sleep(1);
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
