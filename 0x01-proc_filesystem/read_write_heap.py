0f80392daec38a64dbce9b6dcba0447136970821
dreeseh <1859@holbertonschool.com>
2022-06-09 20:51:46 +0000

    back to original

--- 0x01-proc_filesystem/read_write_heap.py
+++ 0x01-proc_filesystem/read_write_heap.py
@@ -2,60 +2,65 @@
 """contains the main method
 """
 
-from re import findall
-from sys import argv
+import sys
 
 
-def read_write_heap(pid, target, replacement):
-    """ finds and replaces target string in heap of a running process"""
-
-    try:
-        with open('/proc/{}/maps'.format(pid), 'r') as maps_file:
-            address_map = maps_file.read()
-            maps_file.close()
-    except FileNotFoundError as e:
-        error_out('{} does not represent a running process.\n'.format(pid))
-
-    pattern = r'\b([\da-zA-Z]+)-([\da-zA-Z]+).*?\[heap]'
-    # re.findall() returns a list of tuples of the captured groups
-    heap_ranges = findall(pattern, address_map)
-    with open('/proc/{}/mem'.format(pid), 'r+b', 0) as mem_file:
-
-        for heap_range in heap_ranges:
-            start = int(heap_range[0], 16)
-            end = int(heap_range[1], 16)
-            mem_file.seek(start)
-            buffer = mem_file.read(end - start)
-            target_location = buffer.find(target)
-
-            if target_location != -1:
-                mem_file.seek(start + target_location)
-                difference = max(len(target) - len(replacement), 0)
-                mem_file.write(replacement + bytes(difference))
-
-        mem_file.close()
+def print_usage_and_exit():
+    """module for printing usage
+    """
+    print('Usage: {} pid search write'.format(sys.argv[0]))
+    sys.exit(1)
 
 
-def error_out(error_msg):
-    """ prints error message and exits with error code 1 """
-    print(error_msg)
+if len(sys.argv) != 4:
+    print_usage_and_exit()
+
+pid = int(sys.argv[1])
+search_string = sys.argv[2]
+write_string = str(sys.argv[3])
+if write_string == "":
+    write_string = "   "
+
+try:
+    file_maps = open("/proc/{}/maps".format(pid), 'r')
+except Exception as e:
+    print(e)
     exit(1)
 
+try:
+    memory_file = open("/proc/{}/mem".format(pid), 'r+b', 0)
+except Exception as e:
+    print(e)
+    exit(1)
 
-if __name__ == '__main__':
+heap_found = False
+for line in file_maps:
+    s_line = line.split(' ')
+    if "heap" in line:
+        heap_found = True
+        print("found HEAP")
 
-    if len(argv) != 4:
-        error_out('Usage: {} pid target_str replacement_str\n'.format(argv[0]))
+        memory_range = s_line[0].split('-')
+        memory_start = int(memory_range[0], 16)
+        memory_end = int(memory_range[1], 16)
+        memory_file.seek(memory_start)
+        memory_search = memory_file.read(memory_end - memory_start)
+        index_search = memory_search.find(bytes(search_string, 'ASCII'))
 
-    try:
-        pid = int(argv[1])
-    except ValueError:
-        error_out('{}: first arg must be valid proccess id\n'.format(argv[0]))
+        if index_search == -1:
+            print("break one")
+            break
 
-    target = bytes(argv[2], 'ascii')
-    replacement = bytes(argv[3], 'ascii')
+        memory_file.seek(memory_start + index_search)
+        memory_file.write(bytes(write_string, "ASCII"))
+        print("write memory break")
+        break
 
-    # Let's do this!
-    read_write_heap(pid, target, replacement)
-    print('Complete.')
-    exit(0)
+file_maps.close()
+memory_file.close()
+
+if not heap_found:
+    print("heap not found")
+    exit(1)
+
+print("now replacing string in memory: /proc/{}/mem".format(pid))
