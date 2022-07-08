292072510278b9e1d8d5d20aafabc5be415b6fe8
dreeseh <1859@holbertonschool.com>
2022-07-08 13:04:28 +0000

    new main file

--- 0x01-proc_filesystem/main.c
+++ 0x01-proc_filesystem/main.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 /**
- * main - uses strdup to create a new string, loops forever-ever
- * Return: EXIT_FAILURE if malloc failed. Other never returns
+ * main - uses strdup to create a new string, and prints the
+ * address of the new duplcated string
+ *
+ * Return: EXIT_FAILURE if malloc failed. Otherwise EXIT_SUCCESS
  */
 int main(void)
 {
 	char *s;
-	unsigned long int i;
 
 	s = strdup("Holberton");
 	if (s == NULL)
@@ -18,12 +18,6 @@ int main(void)
 		fprintf(stderr, "Can't allocate mem with malloc\n");
 		return (EXIT_FAILURE);
 	}
-	i = 0;
-	while (s)
-	{
-		printf("[%lu] %s (%p)\n", i, s, (void *)s);
-		sleep(1);
-		i++;
-	}
+	printf("%p\n", (void *)s);
 	return (EXIT_SUCCESS);
 }
