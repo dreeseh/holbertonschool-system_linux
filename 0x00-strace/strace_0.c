fff40825e86bf233784538eafffdee67df696c91
dreeseh <1859@holbertonschool.com>
2023-01-11 19:09:25 +0000

    moved fflush() outside print loop

--- 0x00-strace/strace_0.c
+++ 0x00-strace/strace_0.c
@@ -1,7 +1,6 @@
 #include "syscalls.h"
 
 
-
 /**
  * main - a program that executes and traces a given command
  * @argc: the commandline argument count
@@ -11,67 +10,66 @@
  **/
 int main(int argc, char **argv, char **envp)
 {
-	int status, in_syscall = 0;
+	int status;
 	struct user_regs_struct regs;
 	pid_t pid;
-	unsigned long syscall_nb;
+	int syscall_return = 1;
+	int first_syscall = 1;
 
 	/* Validate command-line args here, EXIT_FAILURE if no child command given */
 	if (argc < 2)
 	{
-		fprintf(stderr, "Usage: %s <full_path> [path_args]\n", argv[0]);
-		return (1);
+		printf("Usage: %s command [args...]\n", argv[0]);
+		return (EXIT_FAILURE);
 	}
 
 	pid = fork();
+	if (pid == -1)
+	{
+		dprintf(STDERR_FILENO, "Fork failed: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
 
 	if (pid == 0)
 	{
 		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-		execve(argv[1], argv + 1, envp);
-		return (1);
+
+		if (execve(argv[1], argv + 1, envp) == -1)
+		{
+			dprintf(STDERR_FILENO, "Exec failed: %d\n", errno);
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	else if (pid > 0)
 	{
-		/* In parent process, intercept child's syscalls */
-		/* Wait for execve to finish */
-		wait(&status);
-
-		/* Child-process status has 16 significant bits
-		 * Upper 8 bits indicate the signal (we want SIGTRAP)
-		 * Lower 8 indicate child is stopped if equal to 0177 (octal)
-		 * Alternatively, you could just use `while(status == 1407)` but that's even more opaque
-		 */
-		while ((status >> 8) == SIGTRAP && (status & 0xFF) == 0177)
+		while (1)
 		{
-			if (!in_syscall)
+			if (wait(&status) == -1)
+				return (1);
+			if (WIFEXITED(status))
+				break;
+
+			if (!syscall_return || first_syscall)
 			{
-				/* Stopped after the syscall, get register data here */
-				/* Get register values */
-				ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+				if (ptrace(PTRACE_GETREGS, pid,
+				   	NULL, &regs) == -1)
+					return (1);
 
-				/* Syscall number in orig_rax */
-				syscall_nb = (unsigned long)regs.orig_rax;
-
-				/* Print syscall number*/
-				printf("%lu\n", syscall_nb);
-				in_syscall = 1;
-
-				/* For parts 1-8...
-				 * You'll use the syscall number to find data in syscalls_64_g array
-				 */
-			}else
-			{
-				/* Stopped during the syscall, get data on next interation */
-				in_syscall = 0;
+				printf("%lu\n", (unsigned long)regs.orig_rax);
+				first_syscall = 0;
 			}
+			fflush(stdout);
 
-			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-			wait(&status);
-		}	
+		if (ptrace(PTRACE_SYSCALL, pid, NULL, NULL) == -1)
+			return (1);
+
+		/* wait will return after every syscall entry and exit */
+		syscall_return = syscall_return ? 0 : 1;
+		}
 	}
-	else {
+	else
+	{
 		/* Fork failed, EXIT_FAILURE here */
 		perror("fork failed");
 		exit(EXIT_FAILURE);
