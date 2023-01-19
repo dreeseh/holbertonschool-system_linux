0cb9e3d876a1a7831364b8a20d1cd7c4d0bcef3b
dreeseh <1859@holbertonschool.com>
2023-01-19 06:30:20 +0000

    files for task 5

---
+++ 0x01-multithreading/21-prime_factors.c
@@ -0,0 +1,36 @@
+#include "multithreading.h"
+
+/**
+ * prime_factors - factorizes a number into a list of prime factors
+ * @s: the string representation of the number to factorize
+ * Return: list_t list of prine factors
+ */
+list_t *prime_factors(char const *s)
+{
+	unsigned long num;
+	unsigned long *temp, prime = 2;
+	list_t *list = malloc(sizeof(list_t));
+
+	list_init(list);
+
+	num = strtoul(s, NULL, 10);
+	while (prime * prime <= num)
+	{
+		while (num % prime == 0)
+		{
+			temp = malloc(sizeof(unsigned long));
+			*temp = prime;
+			list_add(list, (void *)temp);
+			num /= prime;
+		}
+		prime += 1 + (prime != 2);
+	}
+
+	if (num >= 2)
+	{
+		temp = malloc(sizeof(unsigned long));
+		*temp = num;
+		list_add(list, (void *)temp);
+	}
+	return (list);
+}
