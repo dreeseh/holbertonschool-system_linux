57eafad837f85281a3d184a46ef6e36b5cc3afef
dreeseh <1859@holbertonschool.com>
2023-02-18 01:58:09 +0000

    removed lines for betty

--- 0x00-sockets/0-server.c
+++ 0x00-sockets/0-server.c
@@ -17,50 +17,30 @@ int main(void)
 	int fd_socket, fd_connect, n;
 	struct sockaddr_in servaddr, cliaddr;
 	char buffer[1024];
-
 	/** create the socket**/
 	fd_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (!fd_socket)
-	{
-		printf("create socket failed");
 		exit(0);
-	}
-
 	/** set server address */
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(12345);
-
 	/** bind socket to server address **/
 	if ((bind(fd_socket, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-	{
-		printf("Socket bind failed.\n");
 		exit(0);
-	}
-
-	/** listen for incoming connections **/
-	if ((listen(fd_socket, 5)) != 0)
-	{
-		printf("listen is failing");
+	if ((listen(fd_socket, 5)) != 0) /** listen for incoming connections **/
 		exit(0);
-	}
 	else
-	{
 		printf("Server listening on port 12345");
-	}
-
-	/** accept incoming connections **/
-	len = sizeof(cliaddr);
+	len = sizeof(cliaddr); /** accept incoming connections **/
 	fd_connect = accept(fd_socket, (struct sockaddr *)&cliaddr, &len);
 	if (fd_connect < 0)
 	{
 		printf("Server accept failed.\n");
 		exit(0);
 	}
-
-	/** read data from client **/
-	while (1)
+	while (1) /** read data from client **/
 	{
 		memset(buffer, 0, sizeof(buffer));
 		n = read(fd_connect, buffer, sizeof(buffer));
@@ -69,12 +49,8 @@ int main(void)
 			printf("Read error.\n");
 			exit(0);
 		}
-
 		printf("Received data: %s\n", buffer);
 	}
-
-	/** close tge socket **/
-	close(fd_socket);
-
+	close(fd_socket); /** close the socket **/
 	return (0);
 }
