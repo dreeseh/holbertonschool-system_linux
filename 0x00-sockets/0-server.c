8afed518f4935b66fb125df76394ec342c097131
dreeseh <1859@holbertonschool.com>
2023-02-18 01:54:00 +0000

    betty fixes

--- 0x00-sockets/0-server.c
+++ 0x00-sockets/0-server.c
@@ -9,18 +9,14 @@
 
 /**
  * main - program entry point
- * @argc: argument count
- * @argv: argument vector
  * Return: 0 on success, 1 upon failure
  */
-int main(int argc, char **argv)
+int main(void)
 {
 	unsigned int len;
-	int fd_socket, fd_connect;
+	int fd_socket, fd_connect, n;
 	struct sockaddr_in servaddr, cliaddr;
 	char buffer[1024];
-	(void) argc;
-	(void) argv; 
 
 	/** create the socket**/
 	fd_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,36 +32,38 @@ int main(int argc, char **argv)
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(12345);
 
-	/** bind socket to server address */
+	/** bind socket to server address **/
 	if ((bind(fd_socket, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 	{
 		printf("Socket bind failed.\n");
 		exit(0);
 	}
 
-	/** listen for incoming connections */
+	/** listen for incoming connections **/
 	if ((listen(fd_socket, 5)) != 0)
 	{
 		printf("listen is failing");
 		exit(0);
-	} else {
+	}
+	else
+	{
 		printf("Server listening on port 12345");
 	}
 
-	 // accept incoming connections
+	/** accept incoming connections **/
 	len = sizeof(cliaddr);
 	fd_connect = accept(fd_socket, (struct sockaddr *)&cliaddr, &len);
 	if (fd_connect < 0)
 	{
-        	printf("Server accept failed.\n");
+		printf("Server accept failed.\n");
 		exit(0);
 	}
 
-	/** read data from client */
+	/** read data from client **/
 	while (1)
 	{
 		memset(buffer, 0, sizeof(buffer));
-		int n = read(fd_connect, buffer, sizeof(buffer));
+		n = read(fd_connect, buffer, sizeof(buffer));
 		if (n < 0)
 		{
 			printf("Read error.\n");
