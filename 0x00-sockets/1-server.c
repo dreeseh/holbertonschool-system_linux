b1a21af65e22d60b4f9c8a3ec8370879b7c12cbb
dreeseh <1859@holbertonschool.com>
2023-02-20 15:33:33 +0000

    removed some error priniting for betty

--- 0x00-sockets/1-server.c
+++ 0x00-sockets/1-server.c
@@ -9,6 +9,10 @@
 
 #define PORT 12345
 
+/**
+ * main - entry point for socket listener
+ * Return: 0 on success, 1 upon failure
+ */
 int main(void)
 {
 	int fd_server, new_socket, opt = 1;
@@ -17,25 +21,17 @@ int main(void)
 
 	fd_server = socket(AF_INET, SOCK_STREAM, 0); /** Create socket */
 	if (fd_server == 0)
-	{
-		perror("socket failed");
 		exit(EXIT_FAILURE);
-	}
 	/** Set socket options to reuse address and port */
 	if (setsockopt(fd_server, SOL_SOCKET, SO_REUSEADDR |
 			SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		perror("setsockopt failed");
 		exit(EXIT_FAILURE);
-	}
 	address.sin_family = AF_INET; /** Bind socket to the specified port */
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 	if (bind(fd_server, (struct sockaddr *)&address, sizeof(address)) < 0)
-	{
-		perror("bind failed");
 		exit(EXIT_FAILURE);
-	}
+
 	if (listen(fd_server, 3) < 0) /** Listen for incoming connections */
 	{
 		perror("listen failed");
@@ -44,8 +40,7 @@ int main(void)
 	printf("Listening on port %d...\n", PORT);
 	while (1) /** Accept incoming connections and print client IP address */
 	{
-		new_socket = accept(fd_server,
-					(struct sockaddr *)&address,
+		new_socket = accept(fd_server, (struct sockaddr *)&address,
 					(socklen_t *)&addrlen);
 		if (new_socket < 0)
 		{
