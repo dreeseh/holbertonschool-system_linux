7899670ce6ed82de5d99b9668cbf4e5f87b19e3d
dreeseh <1859@holbertonschool.com>
2023-02-21 22:43:11 +0000

    task 3

---
+++ 0x00-sockets/3-server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+
+/**
+ * main - entry point for socket listener
+ * Return: 0 on success, 1 upon failure
+ */
+int main(void)
+{
+	int fd_server, new_socket, opt = 1, fd_client_sock;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address), num_bytes;
+	char buffer[1024] = {0};
+
+	fd_server = socket(AF_INET, SOCK_STREAM, 0); /** Create socket */
+	if (fd_server == 0)
+		exit(EXIT_FAILURE);
+	/** Set socket options to reuse address and port */
+	if (setsockopt(fd_server, SOL_SOCKET, SO_REUSEADDR |
+			SO_REUSEPORT, &opt, sizeof(opt)))
+		exit(EXIT_FAILURE);
+	address.sin_family = AF_INET; /** Bind socket to the specified port */
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+	if (bind(fd_server, (struct sockaddr *)&address, sizeof(address)) < 0)
+		exit(EXIT_FAILURE);
+
+	if (listen(fd_server, 3) < 0) /** Listen for incoming connections */
+	{
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Listening on port %d...\n", PORT);
+	while (1) /** Accept incoming connections and print client IP address */
+	{
+		new_socket = accept(fd_server, (struct sockaddr *)&address,
+					(socklen_t *)&addrlen);
+		if (new_socket < 0)
+		{
+			perror("accept failed");
+			exit(EXIT_FAILURE);
+		}
+		printf("Client connected: %s:%d\n",
+			inet_ntoa(address.sin_addr),
+			ntohs(address.sin_port));
+
+		/** Receive data from the client */
+		fd_client_sock = new_socket;
+		if ((num_bytes = read(fd_client_sock, buffer, 1024)) < 0)
+		{
+			perror("read failed");
+			exit(EXIT_FAILURE);
+		}
+
+		/** Print the received message */
+		printf("Message Recieved: %s\n", buffer);
+
+		close(new_socket); /** Close the connection */
+		close(fd_client_sock);
+		exit(EXIT_SUCCESS);
+	}
+}
