90bfcece5651121d403d9914aa9e01df72db1b68
dreeseh <1859@holbertonschool.com>
2023-02-22 04:39:24 +0000

    included struct hostent struct

--- 0x00-sockets/2-client.c
+++ 0x00-sockets/2-client.c
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <netdb.h>
 
 /**
  * main - entry point to progran that connects
@@ -18,23 +19,26 @@ int main(int argc, char **argv)
 {
 	int fd_client;
 	struct sockaddr_in server_address;
+	struct hostent *host_network_addr;
 
 	if (argc != 3)
 	{
 		printf("Usage: %s <host> <port>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	/** create a socket */
-	fd_client = socket(PF_INET, SOCK_STREAM, 0);
+
+	fd_client = socket(AF_INET, SOCK_STREAM, 0); /** create a socket */
 	if (fd_client < 0)
 	{
 		perror("socket create error");
 		exit(EXIT_FAILURE);
 	}
-	/** connect to the server */
-	server_address.sin_family = AF_INET;
+
+	server_address.sin_family = AF_INET; /** connect to the server */
 	server_address.sin_port = htons(atoi(argv[2]));
-	inet_pton(AF_INET, argv[1], &server_address.sin_addr);
+	host_network_addr = gethostbyname(argv[1]);
+	inet_pton(AF_INET, inet_ntoa(*(struct in_addr *)host_network_addr->h_addr),
+					&server_address.sin_addr);
 
 	if (connect(fd_client, (struct sockaddr *)&server_address,
 					sizeof(server_address)) == -1)
@@ -45,9 +49,6 @@ int main(int argc, char **argv)
 
 	fprintf(stdout, "Connected to %s:%s\n", argv[1], argv[2]);
 
-	/** close the socket */
-	close(fd_client);
+	close(fd_client); /** close the socket */
 	return (EXIT_SUCCESS);
-	(void)argv;
-	(void)argv;
 }
