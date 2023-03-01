f9106c7e1ec7f73985e09514ab529db10b781b7f
dreeseh <1859@holbertonschool.com>
2023-03-01 18:33:11 +0000

    revert to 1 printf for key/val

--- 0x00-sockets/7-body_parameters.c
+++ 0x00-sockets/7-body_parameters.c
@@ -33,19 +33,16 @@ int break_print_request(int fd_client_socket)
 		if (!key_end)
 			break;
 		*key_end = '\0';
+		
 		key_end++;
-
 		value_start = key_end;
 		value_end = strchr(value_start, '&');
 		if (value_end)
 			*value_end = '\0';
 		
-		printf("Body param: \"%s\" -> ", key_start);
+		printf("Body param: \"%s\" -> \"%s\"\n", key_start, value_start);
 		value_end++;
 		key_start = value_end;
-		printf("\"%s\"\n", value_start);
-		
-		
 	}
 	
 	return(send_message(fd_client_socket));
